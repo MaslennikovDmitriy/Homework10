9d9d27bc090289f9f54c9f7c2415ba2f7864524c
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-04-15 18:13:27 +0300

    Добавил 2 задачу

---
+++ Homework10/task1_server.cpp
@@ -0,0 +1,54 @@
+﻿#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+
+std::string read_data_until(boost::asio::ip::tcp::socket& socket, int& indicator)
+{
+    boost::asio::streambuf buffer;
+    boost::asio::read_until(socket, buffer, '!');
+    std::string message;
+    std::istream input_stream(&buffer);
+    std::getline(input_stream, message, '!');
+    if (message == "end")
+    {
+        indicator = 2;
+    }
+    else indicator = 1;
+    return message;
+}
+
+int main()
+{
+    const std::size_t size = 30;
+
+    auto port = 3333;
+
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address_v4::any(), port);
+
+    boost::asio::io_service io_service;
+
+    try
+    {
+        boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint.protocol());
+
+        acceptor.bind(endpoint);
+        acceptor.listen(size);
+        boost::asio::ip::tcp::socket socket(io_service);
+        acceptor.accept(socket);
+
+        int indicator = 0;
+        while (indicator != 2)
+        {
+            std::cout << read_data_until(socket, indicator) << std::endl;
+        }
+    }
+    catch (boost::system::system_error& e)
+    {
+        std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what() << std::endl;
+        return e.code().value();
+    }
+    system("pause");
+    return EXIT_SUCCESS;
+}
