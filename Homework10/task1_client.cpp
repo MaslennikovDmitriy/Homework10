4b8297f2f3e7c40ee8dc27e7b0fed3399ba70a37
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-04-18 20:41:02 +0300

    Доработал программу, теперь можно писать и с сервера, и с клиента. Вывод сообщений другого пользователя тоже поддерживается

--- Homework10/task1_client.cpp
+++ Homework10/task1_client.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <thread>
+#include <mutex>
+#include <cstdlib>
+#include <cstring>
 
-void write_data(boost::asio::ip::tcp::socket& socket, std::string name, size_t& indicator)
+std::mutex m_mutex;
+
+void write_data(boost::asio::ip::tcp::socket& name_socket, boost::asio::ip::tcp::socket& message_socket, std::string name, int& indicator)
 {
 	std::string message;
-	while (message != "end!")
+	while (message != "end")
 	{
-		if (indicator == 0)
+		if (indicator == -1)
 		{
 			getline(std::cin, message);
 			indicator++;
@@ -14,7 +20,43 @@ void write_data(boost::asio::ip::tcp::socket& socket, std::string name, size_t& 
 		else
 		{
 			getline(std::cin, message);
-			boost::asio::write(socket, boost::asio::buffer(name + ": " + message));
+			boost::asio::write(name_socket, boost::asio::buffer(name + ':'));
+			boost::asio::write(message_socket, boost::asio::buffer(message + '@'));
+		}
+	}
+}
+
+void read_data_until(boost::asio::ip::tcp::socket& name_socket, boost::asio::ip::tcp::socket& message_socket, std::string my_name, int& indicator)
+{
+	while (indicator != 2)
+	{
+		static int ID = 0;
+		boost::asio::streambuf name_buffer;
+		boost::asio::read_until(name_socket, name_buffer, ':');
+		std::string name;
+		std::istream input_stream(&name_buffer);
+		std::getline(input_stream, name, ':');
+		if (my_name != name)
+		{
+			ID = 1;
+		}
+		if (ID == 1)
+		{
+			std::lock_guard <std::mutex> lock(m_mutex);
+			boost::asio::streambuf buffer;
+			boost::asio::read_until(message_socket, buffer, '@');
+			std::string message;
+			std::istream input_stream(&buffer);
+			std::getline(input_stream, message, '@');
+			if (message == "end")
+			{
+				indicator = 2;
+			}
+			else
+			{
+				std::cout << name + ": " + message << std::endl;
+			}
+			ID = 0;
 		}
 	}
 }
@@ -22,23 +64,24 @@ void write_data(boost::asio::ip::tcp::socket& socket, std::string name, size_t& 
 int main()
 {
 	std::string raw_ip_address = "127.0.0.1";
-	auto port = 3333;
-
+	auto port = 8000;
 	std::string name;
 	std::cout << "Insert your name: ";
 	std::cin >> name;
-	size_t name_indicator = 0;
+	int indicator = -1;
 	try
 	{
 		boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(raw_ip_address), port);
-
 		boost::asio::io_service io_service;
+		boost::asio::ip::tcp::socket name_socket(io_service, endpoint.protocol());
+		boost::asio::ip::tcp::socket message_socket(io_service, endpoint.protocol());
 
-		boost::asio::ip::tcp::socket socket(io_service, endpoint.protocol());
-
-		socket.connect(endpoint);
-
-		write_data(socket, name, name_indicator);
+		name_socket.connect(endpoint);
+		message_socket.connect(endpoint);
+		std::thread writer(write_data, std::ref(name_socket), std::ref(message_socket), name, std::ref(indicator));
+		std::thread reader(read_data_until, std::ref(name_socket), std::ref(message_socket), name, std::ref(indicator));
+		writer.join();
+		reader.join();
 	}
 	catch (boost::system::system_error& e)
 	{
--- Homework10/task2_server.cpp
+++ Homework10/task2_server.cpp
@@ -4,51 +4,95 @@
 #include <string>
 #include <boost/asio.hpp>
 #include <thread>
+#include <mutex>
 
-std::string read_data_until(boost::asio::ip::tcp::socket& socket, int& indicator)
+std::mutex m_mutex;
+
+void write_data(boost::asio::ip::tcp::socket& name_socket, boost::asio::ip::tcp::socket& message_socket, std::string name, int& indicator)
 {
-    boost::asio::streambuf buffer;
-    boost::asio::read_until(socket, buffer, '!');
-    std::string message;
-    std::istream input_stream(&buffer);
-    std::getline(input_stream, message, '!');
-    if (message == "end")
-    {
-        indicator = 2;
-    }
-    else indicator = 1;
-    return message;
+	std::string message;
+	while (message != "end")
+	{
+		if (indicator == -1)
+		{
+			getline(std::cin, message);
+			indicator++;
+		}
+		else
+		{
+			getline(std::cin, message);
+			boost::asio::write(name_socket, boost::asio::buffer(name + ':'));
+			boost::asio::write(message_socket, boost::asio::buffer(message + '@'));
+		}
+	}
+}
+
+void read_data_until(boost::asio::ip::tcp::socket& name_socket, boost::asio::ip::tcp::socket& message_socket, std::string my_name, int& indicator)
+{
+	while (indicator != 2)
+	{
+		static int ID = 0;
+		boost::asio::streambuf name_buffer;
+		boost::asio::read_until(name_socket, name_buffer, ':');
+		std::string name;
+		std::istream input_stream(&name_buffer);
+		std::getline(input_stream, name, ':');
+		if (my_name != name)
+		{
+			ID = 1;
+		}
+		if (ID == 1)
+		{
+			std::lock_guard <std::mutex> lock(m_mutex);
+			boost::asio::streambuf buffer;
+			boost::asio::read_until(message_socket, buffer, '@');
+			std::string message;
+			std::istream input_stream(&buffer);
+			std::getline(input_stream, message, '@');
+			if (message == "end")
+			{
+				indicator = 2;
+			}
+			else
+			{
+				std::cout << name + ": " + message << std::endl;
+			}
+			ID = 0;
+		}
+	}
 }
 
 int main()
 {
-    const std::size_t size = 30;
+	const std::size_t size = 30;
+	auto port = 8000;
+	boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address_v4::any(), port);
+	boost::asio::io_service io_service;
 
-    auto port = 3333;
-    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address_v4::any(), port);
+	std::string name;
+	std::cout << "Insert your name: ";
+	std::cin >> name;
+	int indicator = -1;
+	try
+	{
+		boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint.protocol());
 
-    boost::asio::io_service io_service;
-    try
-    {
-        boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint.protocol());
-        size_t count = 0;
-        acceptor.bind(endpoint);
-        acceptor.listen(size);
-        boost::asio::ip::tcp::socket socket(io_service);
-
-        acceptor.accept(socket);
-
-        int indicator = 0;
-        while (indicator != 2)
-        {
-            std::cout << read_data_until(socket, indicator) << std::endl;
-        }
-    }
-    catch (boost::system::system_error& e)
-    {
-        std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what() << std::endl;
-        return e.code().value();
-    }
-    system("pause");
-    return EXIT_SUCCESS;
+		acceptor.bind(endpoint);
+		acceptor.listen(size);
+		boost::asio::ip::tcp::socket name_socket(io_service);
+		boost::asio::ip::tcp::socket message_socket(io_service);
+		acceptor.accept(name_socket);
+		acceptor.accept(message_socket);
+		std::thread writer(write_data, std::ref(name_socket), std::ref(message_socket), name, std::ref(indicator));
+		std::thread reader(read_data_until, std::ref(name_socket), std::ref(message_socket), name, std::ref(indicator));
+		writer.join();
+		reader.join();
+	}
+	catch (boost::system::system_error& e)
+	{
+		std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what() << std::endl;
+		return e.code().value();
+	}
+	system("pause");
+	return EXIT_SUCCESS;
 }
